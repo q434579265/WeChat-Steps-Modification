97ecebeda9f21b7551a73876fc6b3b25f09daf99
q434579265 <634655972@qq.com>
2025-04-27 17:24:56 +0800

    Update steps.py

--- steps.py
+++ steps.py
@@ -43,8 +43,8 @@ def modify_steps(account, password, min_steps, max_steps, attempts=3, timeout=20
 
 # 主程序
 def main():
-    min_steps = 88777
-    max_steps = 88874
+    min_steps = 79898
+    max_steps = 80000
 
     for account, password in account_password_pairs:
         result = modify_steps(account, password, min_steps, max_steps)
